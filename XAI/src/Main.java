64090afbd7eed9cd2080b4f48d846c6e650850d8
adrie <aj.bersia@student.maastrichtuniversity.nl>
2024-09-17 11:44:28 +0200

    Created XAI module

---
+++ XAI/src/Main.java
@@ -0,0 +1,25 @@
+import app.DesktopApp;
+
+public class Main {
+    private static DesktopApp desktopApp = null;
+
+    public static void main(final String[] args)
+    {
+        // The actual launching
+        if (args.length == 0)
+        {
+            desktopApp = new DesktopApp();
+            desktopApp.createDesktopApp();
+        }
+//        else
+//        {
+//            PlayerCLI.runCommand(args);
+//        }
+    }
+
+    // Used in case any agents need DesktopApp functions.
+    public static DesktopApp desktopApp()
+    {
+        return desktopApp;
+    }
+}
