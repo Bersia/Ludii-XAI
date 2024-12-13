16d1c631389bf50c424530640452be9afd5f5716
Bersia <aj.bersia@student.maastrichtuniversity.nl>
2024-12-13 16:28:00 +0100

    test difference server intellij

--- Database/src/model/GameTrialData.java
+++ Database/src/model/GameTrialData.java
@@ -1,6 +1,12 @@
 package model;
 
+import app.PlayerApp;
+import app.utils.GameUtil;
+import app.utils.ReportMessengerGUI;
+import compiler.Compiler;
 import game.Game;
+import main.options.UserSelections;
+import main.grammar.Description;
 import other.AI;
 import other.GameLoader;
 import other.context.Context;
@@ -15,6 +21,7 @@ import search.mcts.nodes.OpenLoopNode;
 import search.mcts.nodes.StandardNode;
 import search.mcts.playout.HeuristicSampingPlayout;
 import search.mcts.selection.UCB1;
+import main.grammar.Report;
 
 import javax.jdo.annotations.*;
 import java.util.ArrayList;
@@ -65,8 +72,8 @@ public class GameTrialData {
         this.reflectionTime = reflectionTime;
 
         final List<String> options = Arrays.asList("Board size/"+boardSize, "Number of Colours/"+numColours);
-        final Game game = GameLoader.loadGameFromName("SameGame.lud", options);
-
+//        final Game game = GameLoader.loadGameFromName("SameGame.lud", options);
+        final Game game = getGame(boardSize, numColours);
         trial = new Trial(game);
         context = new Context(game, trial);
 
@@ -86,6 +93,42 @@ public class GameTrialData {
         }
 
         model = context.model();
+    }
+
+    private Game getGame(int boardSize, int numColours) {
+        String desc =
+                "(game \"SameGame\"  \n" +
+                        "    (players 1)  \n" +
+                        "    (equipment { \n" +
+                        "        (board (square "+boardSize+" diagonals:Implied)) \n" +
+                        "        (piece \"Ball\" Neutral maxState:"+numColours+") \n" +
+                        "    })  \n" +
+                        "    (rules \n" +
+                        "       (start { \n" +
+                        "           (forEach Site (sites Empty) \n" +
+                        "               (place Random {\"Ball0\"} state:(value Random (range 1 "+numColours+"))) \n" +
+                        "           ) \n" +
+                        "       }) \n" +
+
+                        "                (play (forEach Site (sites Occupied by:Neutral) (move Select (from (site) if: ((toBool (count Sites in: (intersection (sites Direction from:(site) Orthogonal distance:1) (sites State (state at: (site))) ) ) )) ) (then (seq { ((do (and (set Var \"state\" (state at:(last To))) (set Var \"size\" 0)) next: (do (forEach Site (sites Group at:(last To) Orthogonal if:(= (state at:(to)) (var \"state\"))) (remove (site)) (then (set Var \"size\" (+ 1 (var \"size\")))) ) next:(addScore Mover ((^ (- (var \"size\") 2) 2  ) )) (then (set Var \"size\" 0)) ) ) ) ((do (set Var \"row\" 0) next: (while (!= (var \"row\") "+boardSize+") (do ((forEach Site (sites Row  (var \"row\")) (if (is Occupied (site)) (if (!= 0 (count Sites in:(sites LineOfSight Farthest at:(site) S))) (fromTo (from (site)) (to (sites LineOfSight Farthest at:(site) S))) ) ) )) next: (set Var \"row\" (+ (var \"row\") 1)) ) ) )) ((seq { (set Var \"countEmpty\" (count Sites in: (intersection (sites Empty) (sites Row 0) ) )) (while (!= (var \"countEmpty\") 0) (seq { (set Var \"col\" 0) (while (!= (var \"col\") "+boardSize+") (seq { ((if (is Empty (coord row:0 column:((var \"col\")))) (seq { (set Var \"row\" 0) (while (!= (var \"row\") "+boardSize+") (seq { (fromTo (from (coord row:(var \"row\") column:(+ 1 ((var \"col\"))))) (to (coord row:(var \"row\") column:((var \"col\"))))) (set Var \"row\" (+ (var \"row\") 1)) } ) ) } ) ) ) (set Var \"col\" (+ (var \"col\") 1)) } ) ) (set Var \"countEmpty\" (- (var \"countEmpty\") 1)) } ) )  } )) (if (= 0 (count Sites in:(sites Occupied by:Neutral))) (addScore Mover 1000) ) } ) ) ) ) )\n" +
+
+                        "        (end { (if (= 0 (count Sites in:(sites Occupied by:Neutral) ) ) (result Mover Win) )  (if (no Moves Next) (result Mover Loss) )  } )\n" +
+                        "    )\n" +
+                        ")";
+
+        final Description gameDescription = new Description(desc);
+        final Report report = new Report();
+        Game game=null;
+        try
+        {
+            game = (Game)Compiler.compile(gameDescription, null, report, false);
+            return game;
+        }
+        catch (final Exception e)
+        {
+            return game;
+        }
+
     }
 
     public Context getContext() {
